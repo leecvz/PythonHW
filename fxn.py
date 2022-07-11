36107ea7d4b1cde502fa6e9b96eb90f5b466eb88
leecvz <97347307+leecvz@users.noreply.github.com>
2022-07-11 16:37:45 +0800

    draft 1

--- fxn.py
+++ fxn.py
@@ -1,25 +1,30 @@
-boy_list = ["he" , "him" , "his", "Onee-chan"]
-girl_list = ["she" , "her" , "her", "Onii-chan"]
-
 prompt = "\n---> "
 
+#this should allow the user to type in any sentence. so long as "boy" or "girl" is in the sentence, it should proceed.
+
 def ask_gender():
-    sex = input(prompt).lower().split(' ')
-    #for completion him\her
-    new_list = []
+    sex = input(prompt).lower().split(" ")
+    gender_list = []
     for i in sex:
         new = i.strip(' "" .!? ')
-        new_list.append(new)
-    #test value
-    return new_list
+        gender_list.append(new)
 
+    if "boy" in gender_list:
+        return ("he" , "him" , "his", "Onee-chan" , "boy")
+    elif "girl" in gender_list:
+        return ("she" , "her" , "her", "Onii-chan" , "girl")
+    
+    #if neither "boy" or "girl" is in the sentence, the user should be asked again
+    else:
+        print("\nJust choose either boy or girl for now. \n")
+        ask_gender()
 
-print("are you a boy or a girl?")
+print("is Alex a boy or a girl?")
 
-gender = ask_gender()
+pronoun1 , pronoun2 , pronoun3 , pet_name , gender = ask_gender()
 
-print(gender)
-
-
-
-#pronoun1 , pronoun2 , pronoun3 , jp_porn = ask_gender()
+print(f"""
+Alex is a {gender}.
+{pronoun1} has 3 siblings who all calls {pronoun2} {pet_name}.
+Even {pronoun3} mom calls {pronoun2} {pet_name}.
+""")
